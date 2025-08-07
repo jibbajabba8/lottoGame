f1864e2d06e9b70c8a46e0c6af0207d833e721a0
James Coney <b9011981@hallam.shu.ac.uk>
2025-08-07 17:18:37 +0100

    fixed issue, not .length its .size for sets

--- main.js
+++ main.js
@@ -2,26 +2,27 @@
 
 const min = 1;
 const max = 47;
-const exclusions = [20,25];
-const numbers = []; // new Set();
+let exclusions = [20,25];
+const numbers = new Set();
 
 function RandomNumber(min, max, exclusions) {
 
     // let logicalMax = max - exclusions.length;
-    const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+    let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
 
-    // for(let i = 0; i < exclusions.length; i++) {
-    //     console.log(exclusions[i], randomNumber); 
-    //     if (randomNumber == exclusions[i]) {
-    //         randomNumber++;
-    //     }
-    // }
+    for(let i = 0; i < exclusions.length; i++) {
+        console.log(exclusions[i], randomNumber); 
+        if (randomNumber == exclusions[i]) {
+            randomNumber++;
+            console.log(numbers);
+        }
+    }
     console.log(randomNumber);
     return randomNumber;
 }
 
-while (numbers.length != 6) {
-    numbers.push(RandomNumber(min,max,exclusions));
+while (numbers.size != 6) {
+    numbers.add(RandomNumber(min,max,exclusions));
 }
 
 console.log(numbers);
